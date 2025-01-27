fa51ab230e02d4ca69369d0da4feec4239e07737
satyakamtyagi369 <satyakamtyagi8979@gmail.com>
2025-01-27 21:55:09 +0530

    print n then ith natural number i+1

---
+++ littleelephantandfunction.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class littleelephantandfunction {
+    public static void main(String[] args) {
+        Scanner hariom = new Scanner(System.in);
+        int n= hariom.nextInt();
+        System.out.print(n);
+        for(int i=1;i<n;i++){
+            System.out.print(" "+i);
+        }
+    }
+}
