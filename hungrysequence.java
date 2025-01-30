f8c897a008b76c7bab873e30347f43eeccf0e58a
satyakamtyagi369 <satyakamtyagi8979@gmail.com>
2025-01-30 10:59:45 +0530

    i+1 > i and i+1%i==0 so we add 1000000 + i

---
+++ hungrysequence.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class hungrysequence {
+    public static void main(String[] args) {
+        Scanner hariom = new Scanner(System.in);
+        int n= hariom.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print((i+1000000)+" ");
+        }
+    }
+}
